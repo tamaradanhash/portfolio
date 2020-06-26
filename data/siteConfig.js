c7292bdb085eaa36f597f3cd4736a5da8c65ef2b
Tamara Danhash <tamaradanhash1989@gmail.com>
2020-06-27 00:25:44 +0200

    Add personal data

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
+  siteTitle: "Hi! I'm Tamara!",
   siteDescription: `Create your online curriculum in just a few minutes with this starter`,
   keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
-  authorAvatar: '/images/avatar.jpeg',
+  authorName: 'Tamara Danhash',
+  twitterUsername: 'TDanhash',
+  githubUsername: 'tamaradanhash',
+  authorAvatar: '/images/c.v-picture.png',
   authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
     In 2012 I started working as a support technician and I approached the developers.
     In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
@@ -13,67 +13,70 @@ module.exports = {
   skills: [
     {
       name: 'HTML',
-      level: 70
+      level: 70,
     },
     {
       name: 'CSS',
-      level: 60
+      level: 60,
     },
     {
       name: 'Javascript',
-      level: 50
+      level: 50,
     },
     {
       name: 'NodeJs',
-      level: 40
+      level: 40,
     },
     {
       name: 'React',
-      level: 60
+      level: 60,
     },
     {
       name: 'Git',
-      level: 70
-    }
+      level: 70,
+    },
   ],
   jobs: [
     {
-      company: "Lendico",
+      company: 'Lendico',
       begin: {
         month: 'apr',
-        year: '2018'
+        year: '2018',
       },
       duration: null,
-      occupation: "Frontend developer",
-      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
-  
-    }, {
-      company: "Anapro",
+      occupation: 'Frontend developer',
+      description:
+        'I integrate the Frontend team responsible for developing and maintaining the online lending platform.',
+    },
+    {
+      company: 'Anapro',
       begin: {
         month: 'dec',
-        year: '2016'
+        year: '2016',
       },
       duration: '1 yr e 5 mos',
-      occupation: "Fullstack developer",
-      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
-    }, {
-      company: "Anapro",
+      occupation: 'Fullstack developer',
+      description:
+        'Development and maintenance, corrective and preventive, of web applications for the real estate market.',
+    },
+    {
+      company: 'Anapro',
       begin: {
         month: 'set',
-        year: '2012'
+        year: '2012',
       },
       duration: '4 yrs e 3 mos',
-      occupation: "Support Technician",
-      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
+      occupation: 'Support Technician',
+      description:
+        'Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool.',
     },
     /* ... */
   ],
   social: {
-    twitter: "https://twitter.com/_franciscodf",
-    linkedin: "https://www.linkedin.com/in/santos-francisco",
-    github: "https://github.com/santosfrancisco",
-    email: "yoshi.df@gmail.com"
+    twitter: 'https://twitter.com/TDanhash',
+    linkedin: 'https://www.linkedin.com/in/tamara-danhash/',
+    github: 'https://github.com/tamaradanhash',
+    email: 'tamaradanhash1989@gmail.com',
   },
   siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
   pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
@@ -87,6 +90,6 @@ module.exports = {
     {
       label: 'Francisco Santos',
       url: '/',
-    }
-  ]
+    },
+  ],
 }
